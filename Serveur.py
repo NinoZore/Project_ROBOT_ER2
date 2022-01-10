9b53f3512b837c4645f4827b6b5e6bce772d1e24
NinoZore <ncls.nino13@gmail.com>
2022-01-11 00:26:59 +0100

    dzdz

--- Serveur.py
+++ Serveur.py
@@ -9,7 +9,10 @@ code_recu = "en attente"
 arduinobus = smbus.SMBus(1) # creation du bus i2c   
 recu = 0
 CODE_HTML = 0
-
+Avancer = 0
+Reculer = 0
+TournerGauche = 0
+TournerDroite = 0
 
 @app.route("/")
 def index():
@@ -18,32 +21,65 @@ def index():
 @app.route("/button",methods = ["POST"])#si on va sur /button on
 def bouton():
     global code_recu
+    global Avancer
+    global Reculer
+    global TournerGauche
+    global TournerDroite
     print(request.get_json())
         
     bouton_appuyer = request.get_json()
     if bouton_appuyer == "AVANCER":
-        #arduinobus.write_byte(addrD, 81)
-        #arduinobus.write_byte(addrG, 81)
-        code_recu = "CODE I2C GAUCHE = 81 et CODE I2C DROITE = 81"
+        if Avancer == 0:  
+            #arduinobus.write_byte(addrD, 81)
+            #arduinobus.write_byte(addrG, 81)
+            code_recu = "CODE I2C GAUCHE = 81 et CODE I2C DROITE = 81"
+            Avancer = 1
+        else :
+            #arduinobus.write_byte(addrD, 80)
+            #arduinobus.write_byte(addrG, 80)
+            code_recu = "CODE I2C GAUCHE = 80 et CODE I2C DROITE = 80"
+            Avancer = 0
         print("avance")   
 
     if bouton_appuyer == "RECULER":
-        #arduinobus.write_byte(addrD, 79)
-        #arduinobus.write_byte(addrG, 79)
-        code_recu = "CODE I2C GAUCHE = 79 et CODE I2C DROITE = 79"
+        if Reculer == 0: 
+            #arduinobus.write_byte(addrD, 79)
+            #arduinobus.write_byte(addrG, 79)
+            code_recu = "CODE I2C GAUCHE = 79 et CODE I2C DROITE = 79"
+            Reculer = 1
+        else :
+            #arduinobus.write_byte(addrD, 80)
+            #arduinobus.write_byte(addrG, 80)
+            code_recu = "CODE I2C GAUCHE = 80 et CODE I2C DROITE = 80"
+            Reculer = 0
         print("recule")     
 
     if bouton_appuyer == "TOURNER GAUCHE":
-        #arduinobus.write_byte(addrD, 81)
-        #arduinobus.write_byte(addrG, 79)
-        code_recu = "CODE I2C GAUCHE = 79 et CODE I2C DROITE = 81"
+        if TournerGauche == 0:
+            #arduinobus.write_byte(addrD, 81)
+            #arduinobus.write_byte(addrG, 79)
+            code_recu = "CODE I2C GAUCHE = 79 et CODE I2C DROITE = 81"
+            TournerGauche = 1
+        else :
+            #arduinobus.write_byte(addrD, 80)
+            #arduinobus.write_byte(addrG, 80)
+            code_recu = "CODE I2C GAUCHE = 80 et CODE I2C DROITE = 80"
+            TournerGauche = 0
         print("tourne gauche")  
           
     if bouton_appuyer == "TOURNER DROITE":
-        #arduinobus.write_byte(addrD, 79)
-        #arduinobus.write_byte(addrG, 81)
-        code_recu = "CODE I2C GAUCHE = 81 et CODE I2C DROITE = 79"
+        if TournerDroite == 0
+            #arduinobus.write_byte(addrD, 79)
+            #arduinobus.write_byte(addrG, 81)
+            code_recu = "CODE I2C GAUCHE = 81 et CODE I2C DROITE = 79"
+            TournerDroite = 1
+        else:
+            #arduinobus.write_byte(addrD, 80)
+            #arduinobus.write_byte(addrG, 80)
+            code_recu = "CODE I2C GAUCHE = 80 et CODE I2C DROITE = 80"
+            TournerDroite = 0
         print("tourne droite")
+        
     return ("un truc")
 
 @app.route("/code")#si on va sur /message on retourne le json { "message": "nouvelle valeur"}
@@ -59,7 +95,6 @@ def WEB():
         #arduinobus.write_byte(addr, 101)
         #time.sleep(0.5)
         #battery = arduinobus.read_byte(addr)
-        time.sleep(0.2)
 
 threadWEB = threading.Thread(target=WEB)
 threadWEB.start()
