4d8df3d4fd73522d249da0e46dbe7022c4737b6e
NinoZore <ncls.nino13@gmail.com>
2022-01-11 17:06:06 +0100

    byvhv

--- Serveur/serveur_http.c
+++ Serveur/serveur_http.c
@@ -37,14 +37,15 @@ int main()
 	char page_html[300];
 	char fichier_html[1500];
 	char version_http[100];
-	
-	char test[]="journal.txt";
-    FILE *Fichier;
+
+    int Fichier;
   
     char find[100];
     char CODE81[] = "81";
     char CODE80[] = "80";
     char CODE79[] = "79";
+	
+	
 	
 		
 	// Creation du socket d'écoute pour le serveur
@@ -174,25 +175,30 @@ int main()
 			close(sock_com);
 			printf("\n Fin de communication avec la connexion numero : %d\n", nb_connexion);
 			printf("----------------------------------------------------------------\n");
-		    
-			Fichier = fopen(test, "r");
-			if (!Fichier)printf("\aERREUR: Impossible d'ouvrir le fichier: %s.\n", test);
-			
-				while (fgets(find,100,Fichier) != NULL)
-				{
-					if (strstr(find, CODE81) != NULL){
-					printf("81 trouvé");
-					}
-					if (strstr(find, CODE80) != NULL){
-					printf("80 trouvé");
-					}
-					if (strstr(find, CODE79) != NULL){
-					printf("79 trouvé");
-					}
-				}
-				fclose(Fichier);
-			}
 		}
+	printf("1\n");
+	Fichier = open(JOURNAL, O_RDWR);
+	printf("2\n");
+    if (!Fichier)printf("\aERREUR: Impossible d'ouvrir le fichier: %s.\n", JOURNAL);
+    printf("3\n");
+while (gets(find,100,Fichier) != NULL)
+	printf("4\n");
+    {
+        if (strstr(find, CODE81) != NULL){
+        printf("81 trouvé");
+
+      }
+        if (strstr(find, CODE80) != NULL){
+        printf("80 trouvé");
+
+      }
+        if (strstr(find, CODE79) != NULL){
+        printf("79 trouvé");
+
+      }
+	  close(Fichier);
+    }
+	}
 	
 	close(sock_ecoute);  	// jamais atteint !!!!
 	return 0;
@@ -279,6 +285,11 @@ int Recherche_Requete_GET(const char *Message, char *page_html, char *version_ht
 	return -1; // Commande non trouvée
 }
 //------------------------------------------------------------------------
+
+
+
+
+
 
 
 
